27e2659ceb32785fecdd962045c66c0a7a911847
Sarah Lin <sarahxwlin@gmail.com>
2026-01-07 13:30:15 -0500

    initial setup

---
+++ backend/src/main/java/com/stashon/primavera_financial/PrimaveraFinancialApplication.java
@@ -0,0 +1,13 @@
+package com.stashon.primavera_financial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimaveraFinancialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimaveraFinancialApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/stashon/primavera_financial/PrimaveraFinancialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stashon.primavera_financial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimaveraFinancialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
